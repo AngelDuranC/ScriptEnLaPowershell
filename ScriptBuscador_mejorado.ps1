125d90e5645dda4086283dea9b5a5ff7fa6fd663
AngelDuranC <45163647+AngelDuranC@users.noreply.github.com>
2018-12-10 22:53:53 +0100

    1
    1

---
+++ ScriptBuscador_mejorado.ps1
@@ -0,0 +1,7 @@
+﻿#Hacer una búsqueda en el buscador chrome
+do{ 
+Write-Host "Busqueda en Google"
+$busqueda = Read-Host "¿Qué quieres buscar?" 
+Start-Process -FilePath "chrome.exe" http://www.google.com/search?q=$busqueda
+$respuesta = Read-Host "Quieres realizar otra búsqueda (S/N)?" 
+ }while ($respuesta -eq 's')
